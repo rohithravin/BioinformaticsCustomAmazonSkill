504d19ff8e32e38332fd6bd5698ba2347f71f206
adriangerardraj <34865898+adriangerardraj@users.noreply.github.com>
2019-03-27 13:17:37 -0400

    Uploaded lambda function
    Uploaded the lambda function. TODO: import libraries and search the database.

---
+++ lambda_function.py
@@ -0,0 +1,188 @@
+
+import urllib
+from botocore.vendored import requests
+import json
+import sys
+BASE_API_URL = 'http://www.ebi.ac.uk/proteins/api/'
+
+
+
+def lambda_handler(event, context):
+    print(event['request'])
+ 
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event)
+    elif event['request']['type'] == "IntentRequest":
+        return intent_scheme(event)
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_end()
+
+
+def on_start(event):
+   print("Session Started.")
+def on_end():
+    print("session end")
+    
+def intent_scheme(event):
+    
+    intent_name = event['request']['intent']['name']
+
+    if intent_name == "WebSearch":
+        return proteinSearch(event)        
+    elif intent_name in ["AMAZON.NoIntent", "AMAZON.StopIntent", "AMAZON.CancelIntent"]:
+        return stop_the_skill(event)
+    elif intent_name == "AMAZON.HelpIntent":
+        return assistance(event)
+    elif intent_name == "AMAZON.FallbackIntent":
+        return fallback_call(event)
+        
+        
+def stop_the_skill(event):
+    stop_MSG = "Thank you. Bye!"
+    reprompt_MSG = ""
+    card_TEXT = "Bye."
+    card_TITLE = "Bye Bye."
+    return output_json_builder_with_reprompt_and_card(stop_MSG, card_TEXT, card_TITLE, reprompt_MSG, True)
+    
+def assistance(event):
+    assistance_MSG = "You can ask me to search a protein. For example, search cancer."
+    reprompt_MSG = "Do you want to hear more about a particular protein?"
+    card_TEXT = "You've asked for help."
+    card_TITLE = "Help"
+    return output_json_builder_with_reprompt_and_card(assistance_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
+
+def fallback_call(event):
+    fallback_MSG = "I can't help you with that, try rephrasing the question or ask for help by saying HELP."
+    reprompt_MSG = "Do you want to hear more about a particular protein?"
+    card_TEXT = "You've asked a wrong question."
+    card_TITLE = "Wrong question."
+    return output_json_builder_with_reprompt_and_card(fallback_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
+        
+def on_launch(event):
+    print("launching")
+    msg = "Hi, welcome to the Uniprot Protein Search Alexa Skill?"
+    reprompt_msg = "Do you want to hear more about a particular protein?"
+    card_TEXT = "Protein Search"
+    card_TITLE = "Choose a protein."
+    return output_json_builder_with_reprompt_and_card(msg, card_TEXT, card_TITLE, reprompt_msg, False)
+def plain_text_builder(text_body):
+    text_dict = {}
+    text_dict['type'] = 'PlainText'
+    text_dict['text'] = text_body
+    return text_dict
+
+def reprompt_builder(repr_text):
+    reprompt_dict = {}
+    reprompt_dict['outputSpeech'] = plain_text_builder(repr_text)
+    return reprompt_dict
+    
+def card_builder(c_text, c_title):
+    card_dict = {}
+    card_dict['type'] = "Simple"
+    card_dict['title'] = c_title
+    card_dict['content'] = c_text
+    return card_dict   
+def response_field_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value):
+    speech_dict = {}
+    speech_dict['outputSpeech'] = plain_text_builder(outputSpeach_text)
+    speech_dict['card'] = card_builder(card_text, card_title)
+    speech_dict['reprompt'] = reprompt_builder(reprompt_text)
+    speech_dict['shouldEndSession'] = value
+    return speech_dict
+
+def output_json_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value):
+    print("building")
+    response_dict = {}
+    response_dict['version'] = '1.0'
+    response_dict['response'] = response_field_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value)
+    return response_dict
+
+def proteinSearch(event):
+    print("searching")
+    lis = []
+    name=event['request']['intent']['slots']['search']['value']
+    print(name)
+    lis.append(name)
+    reprompt_MSG = "Do you want to hear more about a particular protein?"
+    card_TEXT = "You've picked cancer"
+    card_TITLE = "You've picked cancer"
+    return output_json_builder_with_reprompt_and_card("Searching cancer proteins...", card_TEXT, card_TITLE, reprompt_MSG, False)
+    data = {}
+    data['proteins'] = []
+    for keyword  in lis:
+        parameters = {'offset': 0,'accession' : keyword}
+        header = {'Accept': 'application/json'}
+        response = requests.get(BASE_API_URL + 'proteins', params = parameters, headers = header)
+        if response.status_code != 200:
+            return None
+        json_response = response.json()
+        for y in range(len(json_response)):
+            protein = {}
+            protein['id'] = json_response[y]['accession']
+            protein['protein_name'] = json_response[y]['protein']['recommendedName']['fullName']['value']
+            protein['gene'] = json_response[y]['id']
+            protein['function'] = json_response[y]['comments'][0]['text'][0]['value']
+            go = []
+            pdb_entry = []
+            for x in range(len(json_response[y]['dbReferences'])):
+                if json_response[y]['dbReferences'][x]['type'] == 'PDBsum':
+                    pdb_entry.append([json_response[y]['dbReferences'][x]['id'], 'https://www.ebi.ac.uk/pdbe/entry/pdb/' + str(json_response[y]['dbReferences'][x]['id'])])
+                if (json_response[y]['dbReferences'][x]['type'] == 'GO'):
+                    go.append(json_response[y]['dbReferences'][x]['properties']['term'][2:].title())
+            protein['pdb_entries'] = pdb_entry
+            protein['gene_ontology'] = go
+            data['proteins'].append(protein)
+    print(data)
+    with open('proteins.json', 'w') as outfile:
+        json.dump(data, outfile)
+    print(data)
+    
+"""
+#keyword, protein name, location, discription, organism, proteinID
+#pdb entry id, need to parse xml as well
+def get_protein_ids(query, entry_id = None, organism = None, protein_name = None, go_terms = None, keywords = None ):
+    url  = 'https://www.uniprot.org/uniprot/?query='
+    query = query.split()
+    for q in range(len(query)):
+        if q == 0:
+            url+=  str(query[q])
+        else:
+            url+= '+' + str(query[q])
+   
+    url+= '+reviewed:yes&sort=score'
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup( html ,'html.parser')
+    entries = soup('td', {'class' : 'entryID'})
+    protein_ids = []
+    for entry in entries:
+        protein_ids.append(entry.a.string)
+    return protein_ids
+
+
+def get_proteins (query,num=5, entry_id = None, organism = None, protein_name = None, go_terms = None, keywords = None):
+    if not isinstance(num, int):
+        return -1
+    if not isinstance(query, str):
+        return -1
+    if entry_id is not None and not  isinstance(entry_id, list):
+        return -1
+    if organism is not None and not  isinstance(organism, list):
+        return -1
+    if protein_name is not None and not  isinstance(protein_name, list):
+        return -1
+    if go_terms is not None and not  isinstance(go_terms, list):
+        return -1
+    if keywords is not None and not  isinstance(keywords, list):
+        return -1
+    protein_list  = get_protein_ids(query, entry_id, organism, protein_name, go_terms, keywords)
+    if len(protein_list) > num:
+        protein_list = protein_list[:num]
+    print(protein_list)
+    return proteinSearch(protein_list)
+
+    
+print(get_proteins ('human'))
+#EXPAMPLES
+#print(get_proteins (10, 'insulin', organism = ['human']))
+#print(get_proteins (10, 'lung cancer', organism = ['mouse']))
+"""
